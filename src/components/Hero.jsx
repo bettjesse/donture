d21692659a033c59ac9cc1dc0f22fb61d0be7007
jesse <jessebett1999@gmail.com>
2023-04-29 17:36:34 +0300

    updated hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ import { FaVideo, FaLock, FaShieldAlt } from 'react-icons/fa';
   const Hero = () => {
   return (
       <>
-          <section className="bg-white dark:bg-gray-900">
+          <section className = "   bg-cover bg-center" style={{ backgroundImage: "url('/images/bg.jpg')"}}>  
               <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                   <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                       <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">Security system installed! Discover its features and benefits now.</span>
                       <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
                   </a>
-                  <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Donture systems Innovating for the Future.</h1>
-                  <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">DONTURE SYSTEMS LTD is leader in the safety, security, and surveillance gadgets industry. The company
+                  <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none  md:text-5xl lg:text-5xl dark:text-white">Donture systems Innovating for the Future.</h1>
+                  <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-100">DONTURE SYSTEMS LTD is leader in the safety, security, and surveillance gadgets industry. The company
 has been providing innovative and high-quality solutions to meet the ever-growing security needs of
 individuals and businesses</p>
                   <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
@@ -29,17 +29,17 @@ individuals and businesses</p>
                   <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
   <span className="font-bold text-2xl">Security Systems</span>
   <div className="flex justify-center mt-4">
-    <div className="flex flex-col items-center mx-4  hover:text-gray-800 dark:hover:text-gray-400 ">
-      <FaVideo size={50} className="" />
-      <span className="text-lg mt-2">Surveillance</span>
+    <div className="flex flex-col items-center mx-4  ">
+      <FaVideo size={50} className=" hover:text-blue-400 text-blue-600" />
+      <span className="text-lg mt-2  text-gray-100">Surveillance</span>
     </div>
-    <div className="flex flex-col items-center mx-4 hover:text-gray-800 dark:hover:text-gray-400">
-      <FaLock size={50} className="" />
-      <span className="text-lg mt-2">Access Control</span>
+    <div className="flex flex-col items-center mx-4">
+      <FaLock size={50} className="  hover:text-blue-400 text-blue-600" />
+      <span className="text-lg mt-2  text-gray-100">Access Control</span>
     </div>
-    <div className="flex flex-col items-center mx-4 hover:text-gray-800 dark:hover:text-gray-400 ">
-      <FaShieldAlt size={50} className="" />
-      <span className="text-lg mt-2 ">Alarms</span>
+    <div className="flex flex-col items-center mx-4  ">
+      <FaShieldAlt size={50} className="  hover:text-blue-400 text-blue-600" />
+      <span className="text-lg mt-2 text-gray-100 ">Alarms</span>
     </div>
   </div>
 </div>
